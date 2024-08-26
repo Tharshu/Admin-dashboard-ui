e4f230ba92a83e8582ee7e9516e4cf7a392846f9
TharshiEY <ey.tharshi@gmail.com>
2024-08-26 12:26:56 +0530

    updated toast

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,18 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 // import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
+    provideAnimations(),
+    provideToastr(),
     importProvidersFrom(HttpClientModule),
     provideHttpClient(withFetch(),
+
     // withInterceptors([authInterceptor])
   ),
   ],
