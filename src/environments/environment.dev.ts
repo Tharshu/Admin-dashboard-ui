f32b4dd97c08b440266ef536cfedc1c2c5a32b63
Tharshigan <41715880+Tharshu@users.noreply.github.com>
2024-08-28 12:01:23 +0530

    Update environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,7 +3,7 @@ import { version } from "os";
 // src/environments/environment.prod.ts
 export const environment = {
     dev: true,
-    apiEndpoint: 'http://localhost:8080',
+    apiEndpoint: 'https://13.61.26.168/8080',
     v1: `/api/v1/`,
     // other environment-specific variables
   };
